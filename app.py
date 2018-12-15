2794a8a92f5f2749a6d9f235da41ad9c65b10d90
htomp3 <45737492+htomp3@users.noreply.github.com>
2018-12-15 12:49:26 -0700

    added login functionallity with error message

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import logout_user, login_user, login_required,UserMixin, LoginManager
-from sqlalchemy.exc import SQLAlchemyError
-from werkzeug.routing import ValidationError
 
 app = Flask(__name__,static_url_path='/static')
 app.config['SECRET_KEY'] = 'thisisasecretkeyaaaaaaaaaaaaaaa'
@@ -12,28 +10,29 @@ db = SQLAlchemy(app)
 login_manager=LoginManager(app)
 login_manager.init_app(app)
 
+# class Info(db.Model,UserMixin):
+#     id=db.Column(db.Integer,primary_key=True)
+#     bodyweight=db.Column(db.Integer)
+#     bench=db.Column(db.Integer)
+#     squat=db.Column(db.Integer)
+#     incline=db.Column(db.Integer)
+#     deadlift=db.Column(db.Integer)
+#     owner_id=db.Column(db.Integer,db.ForeignKey('client.id'))
 
-class Client(db.Model, UserMixin):
+class Client(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(80), nullable=False)
     lname = db.Column(db.String(80), nullable=False)
     uname = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-
-
-# class Info(db.Model,UserMixin):
-#     id=db.Column(db.Integer,primary_key=True)
-#     bodyweight=db.Column(db.Integer)
-#     # bench=
-#     # squat=
-#     # incline=
-#     # deadlift=
+    #info=db.relationship('Info',backref='owner')
 
 
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     return render_template("index.html")
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/create', methods=["GET", "POST"])
@@ -53,9 +52,9 @@ def create():
                 #print(newClient)
                 db.session.add(newClient)
                 db.session.commit()
+
         else:
             return render_template("logerr.html")
-        #login_user(newClient)
         return redirect('/login')
     return render_template('create.html')
 
@@ -65,19 +64,35 @@ def read():
     clients = Client.query.all()
     return render_template('read.html', clients=clients)
 
-
+# @app.route('/Info', methods=["GET", "POST"])
+# #@login_required
+# def Info():
+#     if request.form:
+#         print(request.form)
+#         bodyweight = int(request.form['bodyweight'])
+#         bench = int(request.form['bench'])
+#         squat = int(request.form['squat'])
+#         incline = int(request.form['incline'])
+#         deadlift=int(request.form['deadlift'])
+#         newInfo=Info(bodyweight=bodyweight, bench=bench, squat=squat, incline=incline, deadlift=deadlift)
+#         db.session.add(newInfo)
+#         db.session.commit()
+#         return redirect('/read')
+#     return render_template('Info.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method=='POST':
         uname=request.form['uname']
         password=request.form['password']
-        client=Client.query.filter_by(uname=uname).first()
-        if client !=None:
-            if password==client.password:
-                login_user(client)
-                return redirect('/index')
+        client=Client.query.filter_by(uname=uname,password=password).first()
+        if client is None:
+            flash('Username or Password is invalid', 'error')
+            return redirect('/login')
+        login_user(client)
+        return render_template('read.html')
     return render_template('login.html')
+
 
 @app.route('/logout')
 @login_required
